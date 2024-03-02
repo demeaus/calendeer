6cf8305b6eb89a0793c926479a3feee01b960bdd
Demeaus Wong <demeaus.wong@gmail.com>
2024-03-02 10:18:05 -0700

    Events that have expired will be deleted by cron job

--- backend_calendeer/backend_calendeer/settings.py
+++ backend_calendeer/backend_calendeer/settings.py
@@ -138,3 +138,11 @@ CORS_ALLOWED_ORIGINS = [
 CRONJOBS = [
     ('*/1 * * * *', 'calendeer.cron.get_approaching_events', '>> ' + os.path.join(BASE_DIR,'debug.log' + ' 2>&1 '))
 ]
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = 'rmfe zduk ebmg uqrz'
+# # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
--- backend_calendeer/calendeer/cron.py
+++ backend_calendeer/calendeer/cron.py
@@ -3,17 +3,30 @@ from datetime import timedelta
 import datetime
 from .models import Event, EventsInvitees
 
-NOTIFICATION_WARNING = 30
-NOTIFICATION_DELTA = 1
+NOTIFICATION_TIME = 30 # minutes
+NOTIFICATION_DELTA = 1 # minutes
+EXPIRATION_TIME = 24 # hours
+
+
+# def notify():
+#     send_mail(
+#         "Subject",
+#         "Deer dem",
+#         "hsnodgrass1234@gmail.com",
+#         ["demeaus.wong@gmail.com"]
+#     )
+
+# notify()
 
 def get_approaching_events():
     print("====get_approaching_events()====")
-    now_minus = datetime.datetime.now() - timedelta(hours=0, minutes=NOTIFICATION_WARNING)
-    now_minus_delta = datetime.datetime.now() - timedelta(hours=0, minutes=NOTIFICATION_WARNING + NOTIFICATION_DELTA)
+    now_minus = datetime.datetime.now() - timedelta(hours=0, minutes=NOTIFICATION_TIME)
+    now_minus_delta = datetime.datetime.now() - timedelta(hours=0, minutes=NOTIFICATION_TIME + NOTIFICATION_DELTA)
+    now_minus_expiration = datetime.datetime.now() - timedelta(hours=EXPIRATION_TIME, minutes=0)
 
     print("now_minus", now_minus)
 
-    # Get list of events that have a start time 30 or 30 + delta minutes before now, to account for seconds
+    # Get list of events that have a start time x to x + delta minutes before now, to account for seconds
     events = Event.objects.filter(datetime_start__lt=now_minus, datetime_start__gt=now_minus_delta)
     print("events, ", events)
 
@@ -32,5 +45,10 @@ def get_approaching_events():
             users_to_notify.append(host_email)
     print("users_to_notify", users_to_notify)
 
+
     # Send notification to each invitee/host for each event
+    # notify()
+
+    # Delete events that are more than x hours since they ended
+    events = Event.objects.filter(datetime_end__lt=now_minus_expiration).delete()
     
